234171342a16ed46c2741a843b14041956c5ac5a
KDM's PlayGround <dongmin703@naver.com>
2026-04-19 00:46:23 +0900

    제거
    백앤드 기능 제거

--- back/src/routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../controllers/userController');
-const { authenticate } = require('../middleware/auth');
-
-router.get('/profile', authenticate, userController.getProfile);
-router.put('/profile', authenticate, userController.updateProfile);
-
-module.exports = router;
